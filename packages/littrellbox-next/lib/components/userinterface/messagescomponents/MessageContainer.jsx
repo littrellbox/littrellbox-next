3124afdb82d8bee1ccc87b68f59e7c9e160305ab
william341 <wwokwilliam@gmail.com>
2019-12-05 14:05:18 -0700

    hide blocked messages

--- packages/littrellbox-next/lib/components/userinterface/messagescomponents/MessageContainer.jsx
+++ packages/littrellbox-next/lib/components/userinterface/messagescomponents/MessageContainer.jsx
@@ -8,13 +8,13 @@ import { Hidden, Visible, HiddenWithMoveUp } from '../../lib/AnimationStyles'
 
 import Tooltip from '../../lib/Tooltip'
 
-
 class MessageContainer extends React.Component {
   constructor(props) {
     super(props)
     
     this.state = {
       showProfile: false,
+      showBlockedMessages: false
     }
   }
 
@@ -56,6 +56,12 @@ class MessageContainer extends React.Component {
     })
   }
 
+  toggleBlocked() {
+    this.setState({
+      showBlockedMessages: !this.state.showBlockedMessages
+    })
+  }
+
   render() {
     document = this.props.document
     if(!this.props.document) {
@@ -72,41 +78,51 @@ class MessageContainer extends React.Component {
     var date = new Date(this.props.messages[0].createdAt)
 
     var timeOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour:'numeric', minute: 'numeric'}
-     return (
-      <div className="message-container message">
-        <div>
-          <Components.ProfileModal user={this.props.document} style={this.state.showProfile ? Visible : HiddenWithMoveUp}/>
-          <div className="dialog-semi-transparent-background" style={this.state.showProfile ? Visible : Hidden} onClick={() => this.toggleProfile()}/>
+    
+    if(this.props.document && !this.state.showBlockedMessages && this.props.currentUser.lb_usersBlocked.includes(this.props.document._id)) {
+      return (
+        <div className="btn" onClick={() => this.toggleBlocked()}>
+          Show blocked messages from {this.props.document.username}
         </div>
-        {this.props.document && !this.props.document.lb_profilePicture && <div className="message-profile-picture" onClick={() => this.toggleProfile()}/>}
-        {this.props.document && this.props.document.lb_profilePicture && <div className="message-profile-picture" onClick={() => this.toggleProfile()}>
-          <img src={this.props.document.lb_profilePicture} className="message-pfp-image"/>
-        </div>}
-        <div style={{width: "100%"}}>
-          <div className="message-header">
-            <div className="message-userdate">
-              <span className="message-username" onClick={() => this.toggleProfile()}>{document.username} </span>
-              {this.props.document && this.props.document.isAdmin && <Tooltip text="Moderator" className="message-name-icon"><FontAwesomeIcon icon={faUserShield}/></Tooltip>}
-              {this.props.document && (this.props.document._id == this.props.planet.userId) && <Tooltip text="Planet Owner" className="message-name-icon"><FontAwesomeIcon icon={faCrown}/></Tooltip>}
-              <span className="message-date">
-                 - {date.toLocaleDateString(navigator.language, timeOptions)}
-              </span> 
-            </div>
-          </div>
-          <div className="message-content">
-            {this.props.messages.map((message) => <Components.Message 
-              key={message._id} 
-              message={message} 
-              documentId={message.userId}
-              scrollToBottom={() => this.props.scrollToBottom()}
-              scrollToBottomMessageMount={() => this.props.scrollToBottomMessageMount()}
-              forcePositionUpdate={() => this.props.forcePositionUpdate()}
-              isScrolled={this.props.isScrolled}
-            />)}
-          </div>
-        </div>
-      </div>
-     )
+      )
+    }
+
+    return (
+     <div className="message-container message">
+       <div>
+         <Components.ProfileModal user={this.props.document} style={this.state.showProfile ? Visible : HiddenWithMoveUp}/>
+         <div className="dialog-semi-transparent-background" style={this.state.showProfile ? Visible : Hidden} onClick={() => this.toggleProfile()}/>
+       </div>
+       {this.props.document && !this.props.document.lb_profilePicture && <div className="message-profile-picture" onClick={() => this.toggleProfile()}/>}
+       {this.props.document && this.props.document.lb_profilePicture && <div className="message-profile-picture" onClick={() => this.toggleProfile()}>
+         <img src={this.props.document.lb_profilePicture} className="message-pfp-image"/>
+       </div>}
+       <div style={{width: "100%"}}>
+         <div className="message-header">
+           <div className="message-userdate">
+             <span className="message-username" onClick={() => this.toggleProfile()}>{document.username} </span>
+             {this.props.document && this.props.document.isAdmin && <Tooltip text="Moderator" className="message-name-icon"><FontAwesomeIcon icon={faUserShield}/></Tooltip>}
+             {this.props.document && (this.props.document._id == this.props.planet.userId) && <Tooltip text="Planet Owner" className="message-name-icon"><FontAwesomeIcon icon={faCrown}/></Tooltip>}
+             <span className="message-date">
+                - {date.toLocaleDateString(navigator.language, timeOptions)}
+             </span> 
+            {this.props.document && this.props.currentUser.lb_usersBlocked.includes(this.props.document._id) && <span onClick={() => this.toggleBlocked()}> Hide Messages</span>}
+           </div>
+         </div>
+         <div className="message-content">
+           {this.props.messages.map((message) => <Components.Message 
+             key={message._id} 
+             message={message} 
+             documentId={message.userId}
+             scrollToBottom={() => this.props.scrollToBottom()}
+             scrollToBottomMessageMount={() => this.props.scrollToBottomMessageMount()}
+             forcePositionUpdate={() => this.props.forcePositionUpdate()}
+             isScrolled={this.props.isScrolled}
+           />)}
+         </div>
+       </div>
+     </div>
+    )
   }
 }
 
