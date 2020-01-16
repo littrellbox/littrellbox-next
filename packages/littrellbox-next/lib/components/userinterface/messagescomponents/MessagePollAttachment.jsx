a55772708f66a50f4e9dd868daf7c9f12d935113
william341 <wwokwilliam@gmail.com>
2020-01-16 11:26:10 -0700

    poll frontend

--- packages/littrellbox-next/lib/components/userinterface/messagescomponents/MessagePollAttachment.jsx
+++ packages/littrellbox-next/lib/components/userinterface/messagescomponents/MessagePollAttachment.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import { registerComponent, withSingle, withCurrentUser } from 'meteor/vulcan:core'
-
-import { faDownload } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import { HiddenWithMoveUp, Visible, Hidden, PointerEventsWorkaround } from '../../lib/AnimationStyles'
+import { registerComponent, withSingle, withCurrentUser, withUpdate } from 'meteor/vulcan:core'
 
 class MessagePollAttachment extends React.Component {
   constructor(props) {
@@ -12,6 +7,19 @@ class MessagePollAttachment extends React.Component {
     this.state = {
       showFullView: false
     }
+  }
+
+  answerPoll(index) {
+    let documentId = this.props.document._id;
+    let workingObject  = JSON.parse(this.props.document.pollObject);
+    workingObject[index].push(this.props.currentUser.username);
+    let workingString = JSON.stringify(workingObject)
+    this.props.updatePoll({
+      selector: { documentId },
+      data: {
+        pollObject: workingString
+      }
+    })
   }
 
   componentDidUpdate() {
@@ -27,13 +35,19 @@ class MessagePollAttachment extends React.Component {
     return Object.entries(JSON.parse(this.props.document.pollObject))
   }
 
+  isVoted(array) {
+    if(array.includes(this.props.currentUser.username))
+      return " message-attachment-poll-answered"
+    return ""
+  }
+
   render() {
     if(!this.props.loading) {
       return (
         <div className="message-attachment-file">
           <span className="message-attachment-poll-name">{this.props.document.pollText}</span>
           <div className="message-attachment-poll-container">
-            {this.getPollQuestions().map((currentValue, index) => (<div key={index} className="message-attachment-poll-answer">{currentValue[0]}</div>))}
+            {this.getPollQuestions().map((currentValue, index) => (<div key={index} onClick={() => this.answerPoll(currentValue[0])} className={"message-attachment-poll-answer" + this.isVoted(currentValue[1])}>{currentValue[0]}</div>))}
           </div>
         </div>
       )
@@ -43,7 +57,8 @@ class MessagePollAttachment extends React.Component {
 }
 
 const options = {
-  collectionName: "Polls"
+  collectionName: "Polls",
+  queryOptions: { pollInterval: 1000 }
 };
 
-registerComponent({ name: 'MessagePollAttachment', component: MessagePollAttachment, hocs: [withCurrentUser, [withSingle, options]]});
+registerComponent({ name: 'MessagePollAttachment', component: MessagePollAttachment, hocs: [withCurrentUser, [withUpdate, options], [withSingle, options]]});
